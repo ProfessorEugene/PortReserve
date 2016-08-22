9e74f1adb844638943635af0cf8c9f258d405d43
erachitskiy <eugene@rachitskiy.com>
2016-08-22 12:21:31 -0400

    adding @ignore to integration test

--- portreserve-integration-test/src/test/java/com/rachitskillisaurus/portreserve/TrivialServerIntegrationTest.java
+++ portreserve-integration-test/src/test/java/com/rachitskillisaurus/portreserve/TrivialServerIntegrationTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
  * @author Dmitry Spikhalskiy <dspikhalskiy@spikhalskiy.com>
  */
 public class TrivialServerIntegrationTest {
+    @Ignore // ignoring this test since it serves as a demo of PRs failure thus breaking builds
     @Test
     public void serverStartup() throws IOException {
         final PortReservation portReservation = PortReservationProvider.get().reserveOpenPort();
